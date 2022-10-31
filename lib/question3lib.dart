bea427303960a954db04211349139d5111a280ae
PauloBDev <paulo.bernardino2@gmail.com>
2022-10-31 11:14:55 +0000

    tentativa de maps

--- lib/question3lib.dart
+++ lib/question3lib.dart
@@ -7,14 +7,14 @@ class Seguradora {
   int anoDeOrigem = 0;
 
   final Map<int, String> seguradora1 = HashMap();
-  final eguradora1 = {
+  final primseguradora = {
     1: 2022,
     2: 'Seguradora1',
     3: 'murada da seguradora1',
     4: 1994
   };
   final Map<int, String> seguradora2 = HashMap();
-  final eguradora2 = {
+  final segseguradora = {
     1: 2021,
     2: 'Seguradora2',
     3: 'murada da seguradora2',
@@ -27,6 +27,12 @@ class TiposSeguros extends Seguradora {
   String tiposeguro = "";
   double preco = 0;
   double valoranual = 0;
+
+  final Map<int, String> seguro1 = HashMap();
+  final primseguro = {};
+
+  final Map<int, String> seguro2 = HashMap();
+  final segseguro = {};
 }
 
 class Apolices extends TiposSeguros {
@@ -34,20 +40,34 @@ class Apolices extends TiposSeguros {
   int nascencapolice = 0;
   double duracao = 0;
   bool apoliceapolices = true;
+
+  final Map<int, String> apolice1 = HashMap();
+  final primapolice = {};
+
+  final Map<int, String> apolice2 = HashMap();
+  final segapolice = {};
 }
 
 class Tomador extends Seguradora {
   int anotomador = 0;
   String nomeTomador = "";
   String muradaTomador = "";
+
+  final Map<int, String> tomador1 = HashMap();
+  final primtomador = {};
+
+  final Map<int, String> tomador2 = HashMap();
+  final segtomador = {};
 }
 
 class Segurado extends Tomador {
   int anosegurado = 0;
   String nomesegurado = "";
   String tiposegurado = "";
-}
 
-class testedemapa {
-  Map<int, String> seguradora = HashMap();
+  final Map<int, String> segurado1 = HashMap();
+  final primsegurado = {};
+
+  final Map<int, String> segurado2 = HashMap();
+  final segsegurado = {};
 }
