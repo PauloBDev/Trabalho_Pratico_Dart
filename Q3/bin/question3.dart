d69a40cb882775639d78936bfaeba35f4468c774
PauloB <paulo.bernardino2@gmail.com>
2022-11-08 12:17:22 +0000

    todas as opções feitas

--- Q3/bin/question3.dart
+++ Q3/bin/question3.dart
@@ -744,23 +744,108 @@ void main() {
           switch (adicionar) {
             //numero de apolices ativas e inativas
             case 1:
+              print('Nome da Seguradora:');
+              String? snome = stdin.readLineSync();
+              print('Murada da Seguradora:');
+              String? smurada = stdin.readLineSync();
+              print('Criada no ano de:');
+              int sano = int.parse(stdin.readLineSync()!);
+              seguradoras.add(Seguradora(
+                nomeseguradora: snome,
+                muradaseguradora: smurada,
+                ano: sano,
+              ));
               break;
             case 2:
+              print('Nome da Seguradora que pertence:');
+              String? seguroSnome = stdin.readLineSync();
+              print('Nome do Seguro:');
+              String? seguronome = stdin.readLineSync();
+              print('Tipo de Seguro:');
+              String? segurotipo = stdin.readLineSync();
+              tiposeguros.add(TipoDeSeguro(
+                nomeseguradora: seguroSnome,
+                nomeseguro: seguronome,
+                tiposeguro: segurotipo,
+              ));
               break;
             case 3:
+              print('Nome da Seguradora que pertence:');
+              String? tomadorSnome = stdin.readLineSync();
+              print('Nome do Seguro que pertence:');
+              String? tomadorSeguronome = stdin.readLineSync();
+              print('Tipo de Seguro que fez:');
+              String? tomadorSegurotipo = stdin.readLineSync();
+              print('Nome do Tomador:');
+              String? tomadornome = stdin.readLineSync();
+              print('Murada do Tomador:');
+              String? tomadormurada = stdin.readLineSync();
+              print('Idade do Tomador:');
+              int tomadoridade = int.parse(stdin.readLineSync()!);
+              print('Nome do Segurado (se for prório escrever o nome):');
+              String? seguradonome = stdin.readLineSync();
+              print('Tipo de Segurado:');
+              String? seguradotipo = stdin.readLineSync();
+              tomadores.add(Tomador(
+                nomeseguradora: tomadorSnome,
+                nomeseguro: tomadorSeguronome,
+                tiposeguro: tomadorSegurotipo,
+                nometomador: tomadornome,
+                muradatomador: tomadormurada,
+                idadetomador: tomadoridade,
+                nomesegurado: seguradonome,
+                tiposegurado: seguradotipo,
+              ));
               break;
             case 4:
+              print('Nome da Seguradora que pertence:');
+              String? apoliceSnome = stdin.readLineSync();
+              print('Nome do Tomador:');
+              String? apoliceTomadornome = stdin.readLineSync();
+              print('Murada do Tomador:');
+              String? apoliceTomadormurada = stdin.readLineSync();
+              print('Idade do Tomador:');
+              int apoliceTomadoridade = int.parse(stdin.readLineSync()!);
+              print('Nome do Seguro que pertence:');
+              String? apoliceSeguronome = stdin.readLineSync();
+              print('Tipo de Seguro:');
+              String? apoliceTiposeguro = stdin.readLineSync();
+              print('Cobertura dada:');
+              double apolicecobertura = double.parse(stdin.readLineSync()!);
+              print('Valor Anual a ser pago:');
+              double apolicevaloranual = double.parse(stdin.readLineSync()!);
+              print('Nome da Apólice:');
+              String? apolicenome = stdin.readLineSync();
+              print('Duração da Apólice');
+              double apoliceduracao = double.parse(stdin.readLineSync()!);
+              print('Apólice activa? (Y/N)');
+              String condicao = stdin.readLineSync()!.toLowerCase();
+              bool apoliceactive = false;
+              if (condicao == 'y') {
+                apoliceactive = true;
+              }
+              apolices.add(Apolices(
+                nomeseguradora: apoliceSnome,
+                nometomador: apoliceTomadornome,
+                muradatomador: apoliceTomadormurada,
+                idadetomador: apoliceTomadoridade,
+                nomeseguro: apoliceSeguronome,
+                tiposeguro: apoliceSeguronome,
+                cobertura: apolicecobertura,
+                valoranual: apolicevaloranual,
+                nomeapolice: apolicenome,
+                duracao: apoliceduracao,
+                active: apoliceactive,
+              ));
               break;
             case 5:
-              break;
-            case 6:
               print('\n');
               break;
             default:
               print('Invalid choise! Please try again.');
               break;
           }
-        } while (adicionar != 6);
+        } while (adicionar != 5);
         break;
 
       //exit
@@ -821,6 +906,5 @@ void menuAdicionar() {
   print('2. Tipos de Seguros.');
   print('3. Tomadores.');
   print('4. Apólices.');
-  print('5. Segurados.');
-  print('6. <-- Voltar.\n');
+  print('5. <-- Voltar.\n');
 }
